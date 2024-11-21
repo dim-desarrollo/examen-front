f26da8853c4c6b21b943a5d24d34386009d85564
Isaias Romano <isaias.romano99@gmail.com>
2024-11-21 10:48:58 -0300

    estructura

--- tramites-web/src/App.jsx
+++ tramites-web/src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import imgHeader from "../public/Dim-2.png"
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='grid-container'>
+      <header className="header">
+        <img src={imgHeader} className='img-logo' />
+        {/* <p>dasdasdsad</p> */}
+      </header>
+      <main className="body">
+        <div className='section-tramites'>
+          <div className='content-title'>
+            <h3 className='title-web'>Tramites Web</h3>
+          </div>
+          <div className='seccion-cuit'>
+            <p className='p-cuit'>Trabajando con la C.U.I.T N° <b>20-05496076-0</b> </p>
+          </div>
+          <div>
+            <button className='btn-nuevo-tramite'>
+              Nuevo Tramite
+            </button>
+          </div>
+        </div>
+      </main>
+      <footer class="footer">
+
+      </footer>
+    </div>
   )
 }
 
